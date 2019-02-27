6330edb6a6ee2e76ac95627733f701c76dd60243
Fischer <quiqueriaza2010@hotmail.com>
2019-02-27 22:59:49 +0100

    Segunda version con el login creado y contraseñas pasadas a MD5

--- app/src/main/java/com/example/ejerciciologin/MainActivity.java
+++ app/src/main/java/com/example/ejerciciologin/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.ejerciciologin;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
@@ -10,19 +12,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
 
 public class MainActivity extends AppCompatActivity{
+    EditText campoUsuario,campoContraseña;
+
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Variables
+        campoUsuario = (EditText) findViewById(R.id.etUsuario);
+        campoContraseña = (EditText) findViewById(R.id.etContraseña);
+
+        //botones
         Button botonregistro = (Button) findViewById(R.id.bttnRegistro);
+        final Button inicioSesion = (Button) findViewById(R.id.bttnentrar);
+        //Registro
         botonregistro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) { //listener para cuando se pulse el boton
@@ -30,6 +42,61 @@ public class MainActivity extends AppCompatActivity{
                 startActivityForResult(intent, 0); //lo iniciamos
             }
         });
+        //Inicio de Sesion
+        inicioSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { //listener para cuando se pulse el boton
+                iniciarSesion();
+            }
+        });
+    }
+    private void iniciarSesion() {
+
+        /*Para iniciar sesion lo que vamos a hacer es comprobar que existe el usuario en la base de datos
+        y su contraseña correspondiente y las vamos a comparar con los datos introducidos por el usuario.
+        La contraseña ya que una base de datos normal no se almacena las contraseñas en texto plano se
+        se encriptan con md5 y lo que se hace es comparar las cadenas de MD5 que hay en la base de datos con
+        la contraseña que mete el usuario que es convertida al instante a MD5 para su comparacion.*/
+
+            Boolean validacionuser=false;
+            Boolean validacioncontra=false;
+
+            ConexionSQLiteHelper conectar =new ConexionSQLiteHelper(this, "DB_usuarios",null,1);
+            SQLiteDatabase db=conectar.getWritableDatabase();
+            ContentValues values=new ContentValues();
+            Cursor fila = db.rawQuery(
+                    "select usuario,contraseña from usuarios", null);
+            //Validaciones en la base de datos
+            if(fila.moveToFirst()) {
+                do {
+                    if (campoUsuario.getText().toString().equals(fila.getString(0))) {
+                        validacionuser = true;
+
+                        if (Registro.md5(campoContraseña.getText().toString()).equals(fila.getString(1))) {
+                            validacioncontra = true;
+                        }
+                    }
+                } while (fila.moveToNext());
+            }
+            //Si el usuario y la contraseña han sido encontrados en la base de datos se procede al login
+                    if(validacionuser==true){
+                        if(validacioncontra==true){
+                            Toast.makeText(getApplicationContext(),"Entra", Toast.LENGTH_SHORT).show();
+
+                        Intent intent = new Intent(this, com.example.ejerciciologin.MenuUsuario.class); //con el intent hacemos que con el click vaya a Registro.class
+                            startActivityForResult(intent, 0); //lo iniciamos
+                        }else{
+                            Toast.makeText(getApplicationContext(), "La contraseña no es correcta", Toast.LENGTH_SHORT).show();}
+
+
+                    }else{
+                        Toast.makeText(getApplicationContext(), "El usuario no es correcto", Toast.LENGTH_SHORT).show();}
+
     }
 
-}
+        }
+
+
+
+
+
--- app/src/main/java/com/example/ejerciciologin/Registro.java
+++ app/src/main/java/com/example/ejerciciologin/Registro.java
@@ -2,6 +2,7 @@ package com.example.ejerciciologin;
 
 import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RadioButton;
@@ -18,14 +20,20 @@ import android.widget.Toast;
 
 import com.example.ejerciciologin.Utilidades.Utilidades;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class Registro extends AppCompatActivity {
 
     TextView estado;
     Spinner comboDias;
     EditText campoUsuario,campoContraseña;
     RadioButton r1,r2;
+    CheckBox privacidad;
     String eleccion;
-
+    String contraseña;
+    Boolean validacionuser=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,12 +45,13 @@ public class Registro extends AppCompatActivity {
         //base de datos
         ConexionSQLiteHelper conectar =new ConexionSQLiteHelper(this, "DB_usuarios",null,1);
 
-        //Errores
-        estado=findViewById(R.id.tv3);
         //Spinner
         comboDias=findViewById(R.id.idSpinner);
         ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this,R.array.paises,android.R.layout.simple_spinner_item);
         comboDias.setAdapter(adapter);
+        //politica de privacidad
+
+        privacidad=findViewById(R.id.cbcondiciones);
 
 
     }
@@ -50,7 +59,36 @@ public class Registro extends AppCompatActivity {
         registrarUsuarios();
     }
 
+    //Contraseñas MD5
+    public static final String md5(final String s) {
+        final String MD5 = "MD5";
+        try {
+            // Create MD5 Hash
+            MessageDigest digest = java.security.MessageDigest
+                    .getInstance(MD5);
+            digest.update(s.getBytes());
+            byte messageDigest[] = digest.digest();
+
+            // Create Hex String
+            StringBuilder hexString = new StringBuilder();
+            for (byte aMessageDigest : messageDigest) {
+                String h = Integer.toHexString(0xFF & aMessageDigest);
+                while (h.length() < 2)
+                    h = "0" + h;
+                hexString.append(h);
+            }
+            return hexString.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    //Registro de usuarios
     private void registrarUsuarios() {
+        Boolean validacionuser=false;
+        contraseña=md5(campoContraseña.getText().toString());
         //Radio Group
         r1=(RadioButton)findViewById(R.id.r1);
         r2=(RadioButton)findViewById(R.id.r2);
@@ -63,13 +101,30 @@ public class Registro extends AppCompatActivity {
         ConexionSQLiteHelper conectar =new ConexionSQLiteHelper(this, "DB_usuarios",null,1);
         SQLiteDatabase db=conectar.getWritableDatabase();
         ContentValues values=new ContentValues();
-        values.put(Utilidades.tabla_usuario,campoUsuario.getText().toString());
-        values.put(Utilidades.tabla_contraseña,campoContraseña.getText().toString());
-        values.put(Utilidades.tabla_nacionalidad,comboDias.getSelectedItem().toString());
-        values.put(Utilidades.tabla_sexo,eleccion);
+        Cursor fila = db.rawQuery(
+                "select usuario from usuarios", null);
+        if(fila.moveToFirst()){
+            do{
+                if(campoUsuario.getText().toString().equals(fila.getString(0))){
+               validacionuser=true;
+                Toast.makeText(getApplicationContext(), "El usuario ya existe,intentelo de nuevo", Toast.LENGTH_SHORT).show();}
+            }while(fila.moveToNext());
+        }
+        if(privacidad.isChecked()) {
+            if (validacionuser == false) {
+                values.put(Utilidades.tabla_usuario, campoUsuario.getText().toString());
+                values.put(Utilidades.tabla_contraseña, contraseña);
+                values.put(Utilidades.tabla_nacionalidad, comboDias.getSelectedItem().toString());
+                values.put(Utilidades.tabla_sexo, eleccion);
+                Long idResultante = db.insert(Utilidades.nombre_tabla, Utilidades.tabla_id, values);
+                Toast.makeText(getApplicationContext(), "id : registro " + idResultante, Toast.LENGTH_SHORT).show();
 
-        Long idResultante=db.insert(Utilidades.nombre_tabla,Utilidades.tabla_id,values);
-        Toast.makeText(getApplicationContext(),"id : registro "+idResultante,Toast.LENGTH_SHORT).show();
+            }
+        }else{
+            Toast.makeText(getApplicationContext(), "Es necesario que acepte las condiciones", Toast.LENGTH_SHORT).show();
+
+        }
+        fila.close();
         db.close();
     }
 }
